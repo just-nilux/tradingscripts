5062b045fb66be30571c21408fcd483f37ef4432
Traderblakeq <liamblakedk@hotmail.com>
2022-11-10 10:12:25 +0100

    more work done on trendl

--- get_trend_line.py
+++ get_trend_line.py
@@ -9,54 +9,56 @@ def detect_peaks(df):
 
     x_peaks = find_peaks_cwt(df.Close, widths=np.arange(5, 15))
 
-    #y_peaks = np.array([])
-
-    #for i in x_peaks:
-    #    y_peaks = np.append(y_peaks, df.iloc[i].Close)
-
-    #peak_list = list(zip(x_peaks, y_peaks))
-
     return x_peaks
 
 
 def all_combi_af_peaks(x_peaks):
 
-    x_peak_combinations_list = np.array([])
+    x_peak_combinations_list = list()
 
     sample_set = set(x_peaks)
 
     for n in range(len(sample_set) +1):
+
         x_peak_combinations_list += list(combinations(sample_set, 3))
 
     x_peak_combinations_list = list(dict.fromkeys(x_peak_combinations_list))
-
-    x_peak_combinations_arr = np.array(x_peak_combinations_list)
     
-    return x_peak_combinations_arr
+    return x_peak_combinations_list
 
 
-def fetch_y_values_peaks(df, x_peak_combinations_arr):
+def fetch_y_values_peaks(df, x_peak_combinations_list):
 
-    y_peaks = np.array([])
+    y_peaks_combination_list = list()
+    
+    for y in x_peak_combinations_list:
 
-    for x in x_peak_combinations_arr:
-        for y in x:
-            temp = list()
-            temp.append(df.iloc[y[0]].Close)
-            temp.append(df.iloc[y[1]].Close)
-            temp.append(df.iloc[y[2]].Close)
-            y_peaks = np.append(y_peaks, temp)
-
-    return y_peaks
+        x1 = df.iloc[y[0]].Close
+        x2 = df.iloc[y[1]].Close
+        x3 = df.iloc[y[2]].Close
+        temp = (x1, x2, x3)
+        y_peaks_combination_list.append(temp)
 
 
-def peak_regression(peak_combinations_list):
+    return y_peaks_combination_list
+
+
+def peak_regression(x_peak_combinations_list, y_peaks_combination_arr):
+
+    # zip de to x / y peaks array sammen inden regression:
+    #x_y_peak_combi_arr = [item for item in zip(x_peak_combinations_list, y_peaks_combination_arr)]
 
     peak_regression_list = list()
 
-    for combination in peak_combinations_list:
+    for i in range(len(x_peak_combinations_list)):
 
-        slope, intercept, r_value, p_value, std_err  = linregress(combination[0], combination[1])
+        slope, intercept, r_value, p_value, std_err  = linregress(x_peak_combinations_list[i], y_peaks_combination_arr[i])
+        x = (slope, intercept, r_value, p_value, std_err )
+        peak_regression_list.append(x)
+
+    return peak_regression_list
+
+
 
 
 
