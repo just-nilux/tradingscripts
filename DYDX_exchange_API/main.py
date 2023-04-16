78bd2e8aec86c023c286eebaf2e44b3593b2d2b7
Traderblakeq <liamblakedk@hotmail.com>
2023-04-16 15:40:23 +0200

    added DYDX exchange communication code

---
+++ DYDX_exchange_API/main.py
@@ -0,0 +1,44 @@
+import json
+import datetime
+import strategy
+import threading
+
+
+from time import sleep
+from connectors import dydx_connector
+from websocket_realtimePrices import RealTimePriceFetcher
+
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+def main():
+
+    # Initialize and start the RealTimePriceFetcher
+    symbols = dydx_connector.get_all_symbols()
+    price_fetcher = RealTimePriceFetcher(symbols)
+    price_fetcher_thread = threading.Thread(target=price_fetcher.start)
+    price_fetcher_thread.start()
+
+    while True:
+        minutes = datetime.datetime.now().minute
+
+        for symbol in range(len(config['time_frame'])):
+            for strat in range(len(config['time_frame'][symbol])):
+                if not(minutes % config['DYDX_TIME_FRAMES'][config['time_frame'][symbol][strat]]):
+                    df = strategy.strategy(dydx_connector.get_klines(config['symbols'][symbol], config['time_frame'][symbol][strat]), symbol, strat)
+
+                    if df['LONG'][len(df) - 1]:
+                        dydx_connector.place_order(config['symbols'][symbol], dydx_connector.order_size(config['symbols'][symbol], symbol, strat), 'BUY', 'MARKET', df)
+                        
+                        sleep(60)
+
+        latest_prices = price_fetcher.get_latest_prices()
+        if isinstance(latest_prices, dict):
+            print(latest_prices['BTC-USD'])
+
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
---
+++ DYDX_exchange_API/websocket_realtimePrices.py
@@ -0,0 +1,76 @@
+from dydx3.helpers.request_helpers import generate_now_iso
+from dydx3.constants import WS_HOST_MAINNET
+from connectors import dydx_connector
+from dydx3 import Client
+import websockets
+import asyncio
+import json
+
+
+
+class RealTimePriceFetcher:
+    def __init__(self, symbols):
+        self.latest_prices = {}
+        self.stop_signal = False
+        self.symbols = symbols
+
+
+    client = dydx_connector.get_client()
+
+    now_iso_string = generate_now_iso()
+
+    signature = client.private.sign(
+        request_path='/ws/accounts',
+        method='GET',
+        iso_timestamp=now_iso_string,
+        data={},
+    )
+
+    req = {
+        'type': 'subscribe',
+        'channel': 'v3_markets',
+        'accountNumber': '0',
+        'apiKey': client.api_key_credentials['key'],
+        'passphrase': client.api_key_credentials['passphrase'],
+        'timestamp': now_iso_string,
+        'signature': signature,
+    }
+
+    async def _fetch_prices(self):
+        try:
+            async with websockets.connect(WS_HOST_MAINNET) as websocket:
+                await websocket.send(json.dumps(self.req))
+
+                while True:
+                    try:    
+                        res = await websocket.recv()
+                        data = json.loads(res)
+
+                        if 'contents' in data:
+                            contents = data['contents']
+                            for sym in self.symbols:
+                                index_price = contents.get(sym, {}).get('indexPrice', None)
+                                if index_price:
+                                    self.latest_prices[sym] = float(index_price)
+                    except (websockets.exceptions.ConnectionClosedError, json.JSONDecodeError) as e:
+                        print(f'Error while receiving data: {e}')
+                        break
+        except websockets.exceptions.WebSocketException as e:
+            print(f'Error while connecting to WebSocket: {e}')
+
+
+    def start(self):
+        self.stop_signal = False
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        asyncio.run(self._fetch_prices())
+        
+    def stop(self):
+        self.stop_signal = True
+
+    def get_latest_prices(self):
+        if self.latest_prices:
+            return self.latest_prices
+        else:
+            return "websocket needs a few second to connect"
+
