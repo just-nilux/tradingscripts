be0186019f37b5758db317ac8b321b5cbe776aea
Traderblakeq <liamblakedk@hotmail.com>
2023-05-23 15:29:44 +0200

    send msg to liq_zone_agent (telegram bot) when liq. zone needs to be updated

--- dydx_communicator/send_telegram_message.py
+++ dydx_communicator/send_telegram_message.py
@@ -5,10 +5,3 @@ def send_telegram_message(bot_token, chat_ids, text):
         send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={text}'
         response = requests.get(send_text)
         return response.json()
-
-# Usage:
-bot_token = '5951325673:AAGvqkhnogQGZPsg8B72xChi5FRsjTEbeDk'
-chat_ids = ['470493359', 'chat_id_2', 'chat_id_3']  # Add chat ids as string here
-text = 'Hello, World!'
-
-send_telegram_message(bot_token, chat_ids, text)
