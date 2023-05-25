1a61d0d92819bdbe63db8164d6149fc1899cafe5
Traderblakeq <liamblakedk@hotmail.com>
2023-05-25 13:45:55 +0200

    changed some logger setup

--- dydx_communicator/logger_setup.py
+++ dydx_communicator/logger_setup.py
@@ -3,22 +3,14 @@ import os
 
 
 def setup_logger(name):
-    """Sets up a logger for a given module.
-
-    Args:
-        name (str): The name of the module.
-
-    Returns:
-        Logger: The logger object.
-    """
-
-
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    log_directory = "system_logs"  # specify your directory here
-    if not os.path.exists(log_directory):  # check if directory exists
-        os.makedirs(log_directory)  # if not, create it
+    # Get the directory where this logger_setup.py file is located
+    log_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "system_log")
+
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
 
     log_file_path = os.path.join(log_directory, f"{name}.log")
 
@@ -29,5 +21,4 @@ def setup_logger(name):
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
-    
     return logger
