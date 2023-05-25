d21966ea35c36d47169641dd33ab967528874e01
Traderblakeq <liamblakedk@hotmail.com>
2023-05-25 13:55:31 +0200

    changed some logger setup

--- dydx_communicator/logger_setup.py
+++ dydx_communicator/logger_setup.py
@@ -1,24 +1,31 @@
 import logging
 import os
 
-
 def setup_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    # Get the directory where this logger_setup.py file is located
     log_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "system_log")
-
     if not os.path.exists(log_directory):
         os.makedirs(log_directory)
 
     log_file_path = os.path.join(log_directory, f"{name}.log")
 
-    handler = logging.FileHandler(log_file_path)
-    handler.setLevel(logging.DEBUG)
+    # Create a file handler
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
 
+    # Create a stream handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and attach it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
     return logger
