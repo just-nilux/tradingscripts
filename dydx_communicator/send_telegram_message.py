207505319b6485d449f4239c1cfc6526b6ff5e8e
Traderblakeq <liamblakedk@hotmail.com>
2023-05-27 14:09:05 +0200

    account equity to TG menu

--- dydx_communicator/send_telegram_message.py
+++ dydx_communicator/send_telegram_message.py
@@ -98,7 +98,7 @@ def process_response(update: Update, context: CallbackContext):
     elif response == 'Account Equity':
         acc_equity = client.fetch_free_equity()
         if acc_equity is not None:
-            update.message.reply_text(text=f"Account Equity: {acc_equity}")
+            update.message.reply_text(text=f"Account Equity: {str(acc_equity)}")
         else:
             update.message.reply_text(text="Failed to fetch account equity.")
 
