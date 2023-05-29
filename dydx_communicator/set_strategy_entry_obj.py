00ace6ad98bc730f974b9fdca91e2b3b4a52df1d
Traderblakeq <liamblakedk@hotmail.com>
2023-05-29 22:05:52 +0200

    change of logger

--- dydx_communicator/set_strategy_entry_obj.py
+++ dydx_communicator/set_strategy_entry_obj.py
@@ -7,7 +7,7 @@ logger = setup_logger(__name__)
 
 def doubleTopEntry(last_closed_candle, detector, ressist_zone_upper, ressist_zone_lower):
 
-    logger.debug(f"Executing doubleTopEntry for detector {doubleTopEntry(__name__)}")
+    logger.debug(f"Executing doubleTopEntry")
 
     detector.resistance_zone_upper = ressist_zone_upper
     detector.resistance_zone_lower = ressist_zone_lower
@@ -22,7 +22,7 @@ def doubleTopEntry(last_closed_candle, detector, ressist_zone_upper, ressist_zon
 
 def doubleBottomEntry(last_closed_candle, detector, support_zone_upper, support_zone_lower):
 
-    logger.debug(f"Executing doubleBottomEntry for detector {doubleBottomEntry(__name__)}")
+    logger.debug(f"Executing doubleBottomEntry")
 
     detector.support_zone_upper = support_zone_upper
     detector.support_zone_lower = support_zone_lower
@@ -36,7 +36,7 @@ def doubleBottomEntry(last_closed_candle, detector, support_zone_upper, support_
 
 def liqSweepEntry(last_closed_candle, detector, upper_liq_level, lower_liq_level):
 
-    logger.debug(f"Executing LiqSweepEntry strategy for {liqSweepEntry(__name__)}")
+    logger.debug(f"Executing LiqSweepEntry strategy")
 
     detector.upper_liq_level = upper_liq_level
     detector.lower_liq_level = lower_liq_level
