76fe3256e43cb748ca6d096527622d1c6b7d9949
Traderblakeq <liamblakedk@hotmail.com>
2023-05-31 08:36:03 +0200

    refactoring of code in generel

--- dydx_communicator/main.py
+++ dydx_communicator/main.py
@@ -309,7 +309,7 @@ def execute_main(client: DydxClient, json_file_path: str, liq_levels: defaultdic
                             send_telegram_message(client.config['bot_token'], client.config['chat_ids'], msg, pass_time_limit=True)
                             res = [pos for pos in client.client.private.get_positions(status='Open').data.get('positions') if pos['market'] == symbol]
                             if res:
-                                position_storage.insert_position(res , entry_strat_type)
+                                position_storage.insert_position(res[0] , entry_strat_type)
                         else:
                             # if not both SL / TP orders have been set, close position:
                             client.cancel_order_by_symbol(symbol)
