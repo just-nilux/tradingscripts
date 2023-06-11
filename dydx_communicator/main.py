6bee194561f2fab530a09005cc8ac3d35e690a5b
Traderblakeq <liamblakedk@hotmail.com>
2023-06-11 16:35:31 +0200

    refactoring

--- dydx_communicator/main.py
+++ dydx_communicator/main.py
@@ -10,12 +10,13 @@ from json_file_processor import process_json_file
 from send_telegram_message import bot_main, send_telegram_message
 from logger_setup import setup_logger
 from dydx_candle_retriever import get_all
+from datetime import datetime
+
 
 import asyncio
 import pandas_ta as ta
 import pandas as pd
 import threading
-import datetime
 import json
 import time
 
@@ -92,12 +93,15 @@ def update_config_with_symbols(data: defaultdict, client: DydxClient):
         if set(new_symbols) != set(strategy['symbols']):
             added_symbols += list(set(new_symbols).difference(strategy['symbols']))  # Compute and store the difference
             strategy['symbols'] = list(set(new_symbols))  # Convert to set and back to list to remove duplicates
-            deactivated_sym = list(set(strategy['symbols']).difference(new_symbols))
+            deactivated_sym += list(set(strategy['symbols']).difference(new_symbols))
 
     # Write back the updated json to file
     if added_symbols:
         with open("config.json", 'w') as json_file:
             json.dump(client.config, json_file, indent=2)
+
+    print(f"added symbols: {added_symbols}")
+    print(f"deactivated symbols: {deactivated_sym}")
 
     return added_symbols, deactivated_sym
 
@@ -143,8 +147,7 @@ def fetch_support_resistance(symbol, liq_levels):
 
 def execute_strategies(client: DydxClient, detectors: dict, atrs: dict, liq_levels: defaultdict(list), first_iteration: bool, symbol: str, timeframe: str, df: pd.DataFrame, signals: list):
 
-    current_time = datetime.datetime.now()
-    minutes = current_time.minute
+    minutes = datetime.now().minute
     timeframe_minutes = timeframe_to_minutes(timeframe)
 
     if not (minutes % timeframe_minutes):
@@ -233,10 +236,10 @@ def execute_main(client: DydxClient, json_file_path: str, position_storage: Posi
         while True:
             try:
                 # Get the current time
-                current_time = datetime.datetime.now()
+                current_second = datetime.now().second
 
                 # Calculate the remaining seconds until the next minute
-                remaining_seconds = 60 - current_time.second
+                remaining_seconds = 60 - current_second
 
                 # Sleep for the remaining seconds
                 time.sleep(remaining_seconds)
@@ -248,13 +251,17 @@ def execute_main(client: DydxClient, json_file_path: str, position_storage: Posi
                     liq_levels = updated_liq_levels
 
                     symbols_added, deactivated_sym = update_config_with_symbols(liq_levels, client)
+                    print(f"print of return deactivated_sym {deactivated_sym}")
                     if symbols_added:
+                        first_iteration = True
                         detectors, atrs = initialize_detectors(client, detectors, atrs)
                         for sym in symbols_added:
                             msg = f"{sym} Activated For Trading"
                             send_telegram_message(msg, pass_time_limit=True)
                     if deactivated_sym:
+                        print(f"deactivated sym before obj_cleanup: {deactivated_sym}")
                         object_cleanup(client, detectors, atrs)
+                        print(f"deactivated sym after ogj_cleanup: {deactivated_sym}")
                         for sym in deactivated_sym:
                             msg = f"{sym} Deactivated For Trading"
                             send_telegram_message(msg, pass_time_limit=True)
@@ -270,19 +277,17 @@ def execute_main(client: DydxClient, json_file_path: str, position_storage: Posi
                 signals = list()
                 for (symbol, timeframe), df in all_symbol_df.items():
                     execute_strategies(client, detectors, atrs, liq_levels, first_iteration, symbol, timeframe, df, signals)
-                    #signals.append(res)
                 logger.debug(f"All orders in each iteration is stored in signals: {signals}")
 
 
                 # if signal: send TG msg. for open orders & save info to DB: ( skal stadig laves)
                 if signals:
-                    #msg = client.fetch_all_open_position(open_pos=sum([len(tup) for tup in signals]))
                     msg = client.fetch_all_open_position(open_pos=len(signals))
                     send_telegram_message(msg, pass_time_limit=True)
 
                     for signal in signals:
                         symbol, tf, entry_strat_type, order = signal
-                        if len(order) == 3:
+                        if isinstance(order, list) and len(order) == 3:
                             res = next((pos for pos in client.client.private.get_positions(status='Open').data.get('positions') if pos['market'] == symbol), None)
                             if res:
                                 position_storage.insert_position(res, entry_strat_type, tf)
